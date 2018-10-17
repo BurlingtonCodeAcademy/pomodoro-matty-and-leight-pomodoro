d6cb8a357b894146d7844c81c25d751f5eb4ade2
Alex Chaffee <alex@stinky.com>
2018-10-17 11:55:53 -0400

    countdown specs

--- cypress/integration/2_countdown.spec.js
+++ cypress/integration/2_countdown.spec.js
@@ -1,25 +1,67 @@
 describe('Countdown', () => {
-  it('starts with 25 minutes on the clock', () => {
-    cy.get('#clock #minutes')
-      .contains('25')
-  });
-  it('starts with 00 minutes on the clock', () => {
-    cy.get('#clock #seconds')
-      .contains('00')
+  describe('starts', () => {
+    it('starts with 25 minutes on the clock', () => {
+      cy.get('#clock #minutes')
+        .contains('25')
+    });
+
+    it('starts with 00 minutes on the clock', () => {
+      cy.get('#clock #seconds')
+        .contains('00')
+    });
   });
 
   describe('when the user clicks Start', () => {
-    beforeEach(()=>{
+    beforeEach(() => {
       cy.clock();
       cy.visit('/');
-    })
-    it('begins to count down once per second', () => {
       cy.get('#start').click();
+    })
+
+    it('begins to count down once per second', () => {
       cy.tick(1000);
       cy.get('#clock #minutes')
         .contains('24')
       cy.get('#clock #seconds')
         .contains('59')
+    });
+
+    it('keeps counting a few more seconds', () => {
+      cy.tick(1000);
+      cy.get('#clock #minutes')
+        .contains('24')
+      cy.get('#clock #seconds')
+        .contains('59')
+
+      cy.tick(1000);
+      cy.get('#clock #minutes')
+        .contains('24')
+      cy.get('#clock #seconds')
+        .contains('58')
+
+      cy.tick(1000);
+      cy.get('#clock #minutes')
+        .contains('24')
+      cy.get('#clock #seconds')
+        .contains('57')
+
+    });
+
+    it('catches up after a few seconds of lag', ()=>{
+      cy.tick(5000);
+      cy.get('#clock #minutes')
+        .contains('24')
+      cy.get('#clock #seconds')
+        .contains('55')
+    });
+
+    it('rolls over the minutes and seconds after 60 ticks', () => {
+      cy.tick(61 * 1000);
+      cy.get('#clock #minutes')
+        .contains('23')
+      cy.get('#clock #seconds')
+        .contains('59')
+
     })
   })
 
