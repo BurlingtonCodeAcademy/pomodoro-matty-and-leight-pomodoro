d0e4e122fb616754bed7c461a2701389d8aa936b
Matty Smith <matthewcsmith18@gmail.com>
2018-10-19 09:09:56 -0400

    Fixed up merge conflicts

--- main.js
+++ main.js
@@ -32,7 +32,47 @@ const formatSeconds = seconds => {
     .toString()
     .padStart(2, "0");
   let remainingSeconds = (seconds % 60).toString().padStart(2, "0");
-  return { minutes, remainingSeconds };
+  return {
+    minutes,
+    remainingSeconds
+  };
+};
+
+flashBackground = () => {
+  let background = document.body.style.backgroundColor;
+  const style = document.body.style;
+  background === ""
+    ? (style.backgroundColor = "red")
+    : (style.backgroundColor = "");
+};
+
+const setButtons = state => {
+  const start = document.querySelector("#start");
+  const pause = document.querySelector("#pause");
+  const reset = document.querySelector("#reset");
+  if (state === "running") {
+    start.disabled = true;
+    pause.disabled = false;
+    reset.disabled = false;
+  } else if (state === "paused") {
+    start.disabled = false;
+    pause.disabled = true;
+    reset.disabled = false;
+  } else if (state === "reset") {
+    start.disabled = false;
+    pause.disabled = true;
+    reset.disabled = false;
+  } else if (state === "finished") {
+    start.disabled = true;
+    pause.disabled = true;
+    reset.disabled = false;
+  }
+};
+
+getInitialSeconds = () => {
+  const minutes = +document.querySelector("#min-in").value;
+  const seconds = +document.querySelector("#sec-in").value;
+  return minutes * 60 + seconds;
 };
 
 flashBackground = () => {
